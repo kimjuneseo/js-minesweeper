68625e2b93ade703eef73a86fe72058559dd2a58
DESKTOP-B35R46I\moongci <dev@l7system.com>
2021-10-25 17:22:22 +0900

    지뢰심기

--- js/app.js
+++ js/app.js
@@ -3,11 +3,22 @@ const gameBoard = document.querySelector('.gameBoard');
 const timer = document.querySelector('.timer');
 const reset = document.querySelector('.reset');
 
-let mine = [];
-let timerSed = 0;
-let sec = 0;
+let mine;
+let mineArr;
+let gameLevel;
+let timerSec = 0;
 let stoptime = true;
 let firstClick = true;
+const levelChangeNum = () => {
+    if (gameLevel === 'easy') return 10;
+    if (gameLevel === 'normal') return 15;
+    if (gameLevel === 'hard') return 20;
+};
+const mineNumMaker = () => {
+    if (gameLevel === 'easy') return 15;
+    if (gameLevel === 'normal') return 30;
+    if (gameLevel === 'hard') return 80;
+};
 
 const startTimer = () => {
     if (stoptime == true) {
@@ -19,94 +30,94 @@ const startTimer = () => {
 const resetTimer = () => {
     stoptime = true;
     timer.innerHTML = '0';
+    timerSec = 0;
 };
 
 const timerCycle = () => {
-    if (stoptime == false ) {
-        sec = sec + 1;
-        timer.innerHTML = sec;
+    if (stoptime == false) {
+        timerSec = timerSec + 1;
+        timer.innerHTML = timerSec;
         let timerSetTime = setTimeout("timerCycle()", 1000);
-        if(stoptime){
-            sec = 0
+        if (stoptime) {
+            timerSec = 0;
             clearInterval(timerSetTime);
         }
     };
 };
 
-const mineMaker = (level) => {
+const gameArrMaker = (level) => {
     mine = [];
     firstClick = true;
     for (let i = 0; i < level; i++) {
         mine.push([]);
         for (let j = 0; j < level; j++) {
-            const isMine = Math.floor(Math.random() * 2 + 1);
-
-            mine[i].push(isMine === 1 ? 1 : 0);
+            mine[i].push(1);
         };
     };
 };
 
-const levelChange = () => {
+const mineMaker = () => {
+    mineArr = []
+    for (let i = 0; i < mineNumMaker(); i++) {
+        let x = Math.floor(Math.random() * levelChangeNum());
+        let y = Math.floor(Math.random() * levelChangeNum());
+        mineArr.push([x, y])
+    }
+    mineArr.forEach((e) => {
+        mine[e[0]][e[1]] = 'e';
+    })
+}
+
+const gameBoardMaker = () => {
     gameBoard.innerHTML = '';
-    mine.forEach((e, idx) => {
-        e.forEach(() => {
-            gameBoard.innerHTML += `
-                    <div class="tile" data-num>
-                        <p class></p>
-                    </div>`;
-        });
+    mine.forEach(e => {
+        e.forEach(e => {
+            e === 'e' ? 
+            gameBoard.innerHTML +=
+                `<div class="tile">
+                <p class="txt_mine"><img src="images/mine.svg" alt="지뢰"></p>
+            </div>` :
+            gameBoard.innerHTML += `<div class="tile">
+            <p class></p>
+            </div>`;
+        })
     });
 };
 
-
-const listener = (e) => {
-    switch (e.target.innerText) {
-        case "easy":
-            gameBoard.style.gridTemplateColumns = "repeat(10, 1fr)";
-            mineMaker(10);
-            levelChange();
-            console.log(mine);
-            break;
-        case "normal":
-
-            mineMaker(15);
-            levelChange();
-            gameBoard.style.gridTemplateColumns = "repeat(15, 1fr)";
-            break;
-        case "hard":
-
-            mineMaker(20);
-            levelChange();
-            gameBoard.style.gridTemplateColumns = "repeat(20, 1fr)";
-            break;
-
-    };
+const frameMaker = () => {
+    gameBoard.style.gridTemplateColumns = `repeat(${levelChangeNum()}, 1fr)`;
+    gameBoard.style.pointerEvents = 'auto';
+    reset.style.pointerEvents = 'auto';
+    
+    gameArrMaker(levelChangeNum());
+    mineMaker();
+    gameBoardMaker();
 };
 
-reset.addEventListener('click', resetTimer);
-
-level.forEach((levels) => {
-    levels.addEventListener("click", listener);
+reset.addEventListener("click", () => {
+    resetTimer();
+    frameMaker();
 });
 
-gameBoard.addEventListener('click', (e) => {
+level.forEach((levels) => {
+    levels.addEventListener("click", e => {
+        gameLevel = e.target.innerText;
+        frameMaker();
+    });
+});
+
+gameBoard.addEventListener("click", (e) => {
     if (firstClick) {
         startTimer();
         firstClick = false;
     };
-    e.target.classList.add('click');
+    e.target.classList.add("click");
 });
 
 gameBoard.addEventListener('contextmenu', (e) => {
     e.preventDefault();
     e.target.classList.toggle('flag');
 });
-
-
-
-
-
-
 
 
 // const evt = new App()
