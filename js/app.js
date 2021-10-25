339fddf44763b905518323fe5674bb8d0ff70b27
kimjuneseo <june3929213512@gmail.com>
2021-10-25 20:09:47 +0900

    집에서 수정

--- js/app.js
+++ js/app.js
@@ -133,22 +133,3 @@ gameBoard.addEventListener('contextmenu', (e) => {
 
 
 
-// const evt = new App()
-// class App{
-//     constructor(){
-//         this.easy 
-//         const level = document.querySelectorAll('.level>button');
-//         // this.level = level
-//         this.addEvent();
-//     };
-
-//      addEvent(){
-//          this.level.addEventListener('click', () => {
-//              console.log(1);
-//          })
-//          console.log(this.level);
-//      }
-// }
-// level.addEventListener('click', (e) => {
-// console.log(e);
-// })
